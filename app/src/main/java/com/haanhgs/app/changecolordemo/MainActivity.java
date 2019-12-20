5752ab951fb427972d8bbc22722b9b4b86be39ad
animevn <haanhth21@gmail.com>
2019-12-20 14:44:15 +0700

    update layout

--- app/src/main/java/com/haanhgs/app/changecolordemo/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/changecolordemo/MainActivity.java
@@ -1,14 +1,33 @@
 package com.haanhgs.app.changecolordemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.lnColor)
+    LinearLayout lnColor;
+    @BindView(R.id.tvColor)
+    TextView tvColor;
+    @BindView(R.id.bnColor)
+    Button bnColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.bnColor)
+    public void onViewClicked() {
     }
 }
