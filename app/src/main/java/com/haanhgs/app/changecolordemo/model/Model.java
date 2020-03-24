2475a2ff2ab39670f73884af933d5aab8e218630
animevn <haanhth21@gmail.com>
2020-03-24 12:23:41 +0700

    refract

--- app/src/main/java/com/haanhgs/app/changecolordemo/model/Model.java
+++ app/src/main/java/com/haanhgs/app/changecolordemo/model/Model.java
@@ -45,5 +45,4 @@ public class Model implements Serializable {
     public int getColorRGB() {
         return colorRGB;
     }
-
 }
--- app/src/main/java/com/haanhgs/app/changecolordemo/viewmodel/ViewModel.java
+++ app/src/main/java/com/haanhgs/app/changecolordemo/viewmodel/ViewModel.java
@@ -72,5 +72,4 @@ public class ViewModel extends AndroidViewModel {
         }
         liveData.setValue(model);
     }
-
 }
