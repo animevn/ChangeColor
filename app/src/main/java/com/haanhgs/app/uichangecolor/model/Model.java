ae867d3466acd91ef178772985d2a308159e6ff8
saigonjew <saigonjew@gmail.com>
2020-10-07 21:37:42 +0700

    reset

--- app/src/main/java/com/haanhgs/app/uichangecolor/model/Model.java
+++ app/src/main/java/com/haanhgs/app/uichangecolor/model/Model.java
@@ -1,48 +1,10 @@
 package com.haanhgs.app.uichangecolor.model;
 
-import android.content.Context;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import androidx.core.content.ContextCompat;
 
 public class Model implements Serializable {
 
-    private final List<String> colorNames = new ArrayList<>();
-    private int iterator = 0;
-    private String colorName;
-    private int colorRes;
-    private int colorRGB;
-
     private static final long serialUID = 220479;
 
-    public void getColorForEachIterate(Context context){
-        colorName = colorNames.get(iterator);
-        colorRes = context.getResources()
-                .getIdentifier(colorName, "color", context.getPackageName());
-        colorRGB = ContextCompat.getColor(context, colorRes);
-    }
 
-    public void iterateThroughColorList(Context context){
-        iterator++;
-        if (iterator == colorNames.size()) iterator = 0;
-        getColorForEachIterate(context);
-    }
-
-    public void setColorNames(String[] colorNames) {
-        Collections.addAll(this.colorNames, colorNames);
-    }
-
-    public String getColorName() {
-        return colorName;
-    }
-
-    public int getColorRes() {
-        return colorRes;
-    }
-
-    public int getColorRGB() {
-        return colorRGB;
-    }
 }
--- app/src/main/java/com/haanhgs/app/uichangecolor/view/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/uichangecolor/view/MainActivity.java
@@ -1,85 +1,15 @@
 package com.haanhgs.app.uichangecolor.view;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import com.haanhgs.app.uichangecolor.R;
-import com.haanhgs.app.uichangecolor.viewmodel.ViewModel;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.ViewModelProvider;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.lnColor)
-    LinearLayout lnColor;
-    @BindView(R.id.tvColor)
-    TextView tvColor;
-    @BindView(R.id.bnColor)
-    Button bnColor;
-
-    private ViewModel viewModel;
-
-    private void hideActionBar(){
-        if (getSupportActionBar() != null) getSupportActionBar().hide();
-    }
-
-    @SuppressWarnings("deprecation")
-    private void hideStatusBar(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            final WindowInsetsController controller = getWindow().getInsetsController();
-            if (controller != null){
-                controller.hide(WindowInsets.Type.statusBars());
-            }
-        }else {
-            getWindow().setFlags(
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN
-            );
-        }
-    }
-
-    private void updateViews(String colorName, int colorRGB, int colorRes){
-        tvColor.setText(colorName);
-        tvColor.setBackgroundColor(colorRGB);
-        lnColor.setBackgroundColor(colorRGB);
-        bnColor.setBackgroundTintList(ContextCompat.getColorStateList(this, colorRes));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        hideActionBar();
-        hideStatusBar();
-        viewModel = new ViewModelProvider(this).get(ViewModel.class);
-        viewModel.getLiveData().observe(this, model ->
-                updateViews(model.getColorName(), model.getColorRGB(), model.getColorRes()));
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        viewModel.loadModel();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        viewModel.saveModel();
-    }
-
-    @OnClick(R.id.bnColor)
-    public void onViewClicked(){
-        viewModel.iterateThrougList();
-    }
 }
